9efc97d2eb33213b46b672f92f8588d28fe8c05f
sriram6129 <sriram9487tk@gmail.com>
2025-01-27 15:04:26 +0530

    V.0.0.0

---
+++ CustomerServiceServer/src/main/java/com/project/two/CustomerServiceServer/CustomerServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.two.CustomerServiceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceServerApplication.class, args);
+	}
+
+}
---
+++ UserServiceServer/src/main/java/com/project/two/UserServiceServer/UserServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.two.UserServiceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceServerApplication.class, args);
+	}
+
+}
